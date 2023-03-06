2845cce4505891fc7c2d250a68ef9dbf961a03f2
Pabearyoga <pabearyoga@gmail.com>
2023-03-06 17:50:15 +0200

    add reducer and store

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from "react";
 
+import { Provider } from "react-redux";
+
 import { NavigationContainer } from "@react-navigation/native";
 
 import { useRoute } from "./router";
+import { store } from "./redux/store";
 
 export default function App() {
   // const routing = useRoute(false);
   const routing = useRoute(true);
 
-  return <NavigationContainer>{routing}</NavigationContainer>;
+  return (
+    <Provider store={store}>
+      <NavigationContainer>{routing}</NavigationContainer>
+    </Provider>
+  );
 }
